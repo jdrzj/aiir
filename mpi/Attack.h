8150a2613ec496f28c3d327e52c4d126d22ae322
tooooomasz <tmsbag@gmail.com>
2015-05-16 21:41:10 +0200

    Brute Force w wersji mpi

---
+++ mpi/Attack.h
@@ -0,0 +1,30 @@
+
+enum class AttackType { dictionary, rainbow, bruteForce};
+
+class Attack {
+    AttackType type;
+    std::string key;
+    HashingFunction function;
+    std::string dictionary_file_name;
+    int chains_range_start;
+    int chains_range_end;
+    int letters_count;
+    int world_rank;
+    const std::string suffixes = "1234567890!@#$^&*()_+{}.";
+
+public:
+    Attack(const AttackType& t, const std::string& k, HashingFunction
+        function);
+    ~Attack();
+    void setDictionaryFileName(const std::string& file_name);
+    std::string defeatKey();
+    void check_suffixes(std::string pass, int level);
+    void setChainsRange(int start, int end);
+    void setLettersCount(int count);
+    void setWorldRank(int rank);
+private:
+    void dictionaryAttack(std::string& file_name);
+    void rainbowAttack();
+    void hackify(std::string pass);
+    std::string bruteForceAttack();
+};
