efae0d238b842bb2a10a3c775d707aad4c51b296
Ola Berjak <berjak.aleksandra@gmail.com>
2015-05-17 13:59:50 +0200

    [django] Poprawki aktualizacji statusu taska i hasla

--- Lamacze_Hasel/aplikacja_kliencka/mpi_listen.py
+++ Lamacze_Hasel/aplikacja_kliencka/mpi_listen.py
@@ -17,14 +17,37 @@ class GetMessages(Thread):
       result['id'] = int(result['id'])
 
       password = Password.objects.filter(id=result['id'])[0]
+      task = Task.objects.filter(id=password.task.id)[0]
+
+      # Dostalismy JSON-a z progressem
       if result.get('password') is None:
-        password.status = F('status') + 1
+
+        # ustawiamy status hasla
+        password.status = int(result['status'])
+        password.save()
+
+        print('Got password status: ', password.status)
+
+        # liczymy status calego zadania
+        passwords_per_task = task.passwords.all().count()
+        print('Passwords per task: ' + str(passwords_per_task))
+        status_sum = 0
+
+        for single_password in task.passwords.all():
+          print ('Passowrd status: ' + str(single_password.status))
+          status_sum += int(single_password.status)
+
+        task.status = int(status_sum / passwords_per_task)
 
       else:
         password.password = result['password']
-
         password.start_time = datetime.fromtimestamp(int(result['start_time']))
         password.end_time = datetime.fromtimestamp(int(result['end_time']))
+
+      # Zakonczenie taska
+      if task.status is 100:
+        task.end_time = password.end_time
       
+      task.save()
       password.save()
-      print('%s received %s' % (self.cluster_id, result))
+      print('Cluster %s received %s' % (self.cluster_id, result))
